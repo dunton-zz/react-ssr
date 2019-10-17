fd0dd09e3cc46ad595ee5dae4c5de89c1a96948d
Ryan Dunton <rdunton@090-eqxcorpML18-3.local>
2019-10-17 14:40:23 -0400

    redux setup for client rendering

--- server/src/helpers/renderer.js
+++ server/src/helpers/renderer.js
@@ -1,11 +1,20 @@
 import React from "react";
 import { renderToString } from "react-dom/server";
 import Home from "../client/components/Home";
+import { StaticRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import Routes from "../client/Routes";
 
 // this separates react server stuff from regular server stuff
 
-export default () => {
-  const content = renderToString(<Home />);
+export default (req, store) => {
+  const content = renderToString(
+    <Provider store={store}>
+      <StaticRouter location={req.path} context={{}}>
+        <Routes />
+      </StaticRouter>
+    </Provider>
+  );
   //generate html doc with script tag
   return `
     <html>
