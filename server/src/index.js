ff6716d2f6b5e5e7a97bd6d8e71e6007ead09bd5
Ryan Dunton <rdunton@090-eqxcorpML18-3.local>
2019-10-21 11:36:58 -0400

    setup data fetching from server

--- server/src/index.js
+++ server/src/index.js
@@ -2,16 +2,20 @@ import "babel-polyfill";
 import express from "express";
 import renderer from "./helpers/renderer";
 import createStore from "./helpers/createStore";
+import { matchRoutes } from "react-router-config";
+import Routes from "./client/Routes";
 const app = express();
 app.use(express.static("public"));
 
 app.get("*", (req, res) => {
   const store = createStore();
+  const promises = matchRoutes(Routes, req.path).map(({ route }) => {
+    return route.loadData ? route.loadData(store) : null;
+  });
 
-  // some logic to initialize and load data into store
-  //
-
-  res.send(renderer(req, store));
+  Promise.all(promises).then(() => {
+    res.send(renderer(req, store));
+  });
 });
 
 app.listen(3000, () => {
