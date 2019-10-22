de5262c6dd6d69e2bb0f2d44544f4cc0f0c42bc8
Ryan Dunton <rdunton@090-eqxcorpML18-3.local>
2019-10-22 11:41:03 -0400

    error handling and admins panes

--- server/src/client/Routes.js
+++ server/src/client/Routes.js
@@ -3,6 +3,7 @@ import App from "./App";
 import HomePage from "./pages/HomePage";
 import UsersListPage, { loadData } from "./pages/UsersListPage";
 import NotFoundPage from "./pages/NotFoundPage";
+import AdminsListPage from "./pages/AdminsListPage";
 
 // setting it up like this to use SSR
 export default [
@@ -17,6 +18,10 @@ export default [
       {
         ...UsersListPage,
         path: "/users"
+      },
+      {
+        ...AdminsListPage,
+        path: "/admins"
       },
       {
         ...NotFoundPage
